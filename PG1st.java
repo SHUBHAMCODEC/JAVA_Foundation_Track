411064fb20944be047f0541daa8742593e7ce149
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-05-17 16:24:18 +0000

    2nd commit

--- PG1st.java
+++ PG1st.java
@@ -4,5 +4,6 @@ class PG1st{
         System.out.println("I LOVE UNIVERSE, and this Universe belogs to me.");
 
         System.out.println("REMEMBER,\n\t You can be the master! of your Soul,captain! of your mind but have this in mind that life! is coming from you not, at you it takes time.");
+        System.out.println("this is the second Commit in Github");
     }
 }
