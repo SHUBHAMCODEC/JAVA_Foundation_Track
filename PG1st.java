65d21e94fadbedec93a8527572519103d121025d
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-05-17 16:09:15 +0000

    second

--- PG1st.java
+++ PG1st.java
@@ -3,6 +3,6 @@ class PG1st{
     public static void main(String args[]){
         System.out.println("I LOVE UNIVERSE, and this Universe belogs to me.");
 
-        System.out.println("REMEMBER,/n/t You can be the master! of your Soul,captain! of your mind but have this in mind that life! is coming from you not, at you it takes time.");
+        System.out.println("REMEMBER,\n\t You can be the master! of your Soul,captain! of your mind but have this in mind that life! is coming from you not, at you it takes time.");
     }
 }
